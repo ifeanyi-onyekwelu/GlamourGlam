99ce99eab12fa8b79ad68081f89ff7b6059e675c
ifekel <ifeanyionyekwelu786@gmail.com>
2023-09-19 20:16:33 +0100

    requirements.txt

--- glamourglam/settings.py
+++ glamourglam/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', "False").lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
+ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS")]
 
 SHIPPING_FEE = os.getenv('SHIPPING_FEE', 10000.00)
 
