11ce9ab7911d1a9e10a5ce281ed149c16b8e6433
Ifeanyichukwu Onyekwelu <ifeanyionyekwelu786@gmail.com>
2025-09-06 17:11:50 +0100

    Added seed data to the management

--- build.sh
+++ build.sh
@@ -5,7 +5,5 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py makemigrations glamourApp
-python manage.py makemigrations users
 python manage.py migrate
 python manage.py seed_data
